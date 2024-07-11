525f464220147e909ccad2afff493083c960cdcc
Vasanth Subramanian <vasant.techie@gmail.com>
2024-07-12 01:02:27 +0530

    Create spark-sql-date-format.sql

---
+++ spark-sql-date-format.sql
@@ -0,0 +1,19 @@
+SELECT
+    CASE WHEN CAST(year_column AS INT) >= YEAR(current_date()) % 100
+         THEN CONCAT('19', LPAD(CAST(year_column AS STRING), 2, '0'))
+         ELSE CONCAT('20', LPAD(CAST(year_column AS STRING), 2, '0'))
+    END AS year_adjusted,
+    LPAD(CAST(month_column AS STRING), 2, '0') AS month_padded,
+    LPAD(CAST(day_column AS STRING), 2, '0') AS day_padded,
+    CONCAT(
+        CASE WHEN CAST(year_column AS INT) >= YEAR(current_date()) % 100
+             THEN CONCAT('19', LPAD(CAST(year_column AS STRING), 2, '0'))
+             ELSE CONCAT('20', LPAD(CAST(year_column AS STRING), 2, '0'))
+        END,
+        '-',
+        LPAD(CAST(month_column AS STRING), 2, '0'),
+        '-',
+        LPAD(CAST(day_column AS STRING), 2, '0')
+    ) AS date_column
+FROM
+    your_table_name;
